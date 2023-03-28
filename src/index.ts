1ac6a51f7230b00da9abe646b4a62903b957de55
AlekPawlowski <alekpawlowski21@gmail.com>
2023-03-28 20:53:27 +0200

    add json server, update gitignore and scripts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello'); 
+import { User } from "./models/User";
+
+const user = new User({name: "John", age: 20});
+user.on('change', () => {
+    console.log('user changed 3');
+});
+
+user.on('change', () => {
+    console.log('user changed 2');
+});
+
+user.on('save', () => {
+    console.log('save was triggered');
+});
+
+user.trigger('save');
