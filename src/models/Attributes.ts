ddcba69abff2a3086d7b2b6686858a101d591d5f
AlekPawlowski <alekpawlowski21@gmail.com>
2023-05-01 12:32:14 +0200

    set up generic classes with getter

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,20 +1,11 @@
 export class Attributes<T> {
     constructor(private data: T) { }
 
-    get(propName: string): string | number | boolean {
-        return this.data[propName];
+    get = <K extends keyof T>(key: K): T[K] => {
+        return this.data[key];
     }
 
-    set(update: T): void {
+    set = (update: T): void => {
         Object.assign(this.data, update);
     }
 }
-
-/* teraz w kursie będzie o rozwiązaniu problemu zwracanego 
-typu w przypadku w którym mamy w zwracanym typie wiecej 
-niz jeden (get method) tak aby było to bardziej generyczne
-oraz abyśmy mogli uzywać bez przeszkód metod dla danych 
-typów (teraz w returnie jak przypiszemy do jakiegoś obiektu/
-zmiennej to będziemy mogli uzywać tylko wspólnych dla danych 
-typów obiektow)
-*/
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 import { Attributes } from "./Attributes";
+
 export interface UserProps {
     name?: string;
     age?: number;
@@ -11,14 +12,25 @@ const url = "http://localhost:3000/users"
 export class User {
     public events: Eventing = new Eventing();
     public sync: Sync<UserProps> = new Sync<UserProps>(url);
+    public attributes: Attributes<UserProps>;
 
-    constructor(private data: UserProps) { }
-
-    get(propName: string): string | number {
-        return this.data[propName];
+    constructor(attrs: UserProps) {
+        this.attributes = new Attributes<UserProps>(attrs);
     }
 
-    set(update: UserProps): void {
-        Object.assign(this.data, update);
+    get on() {
+        // return reference to the events.on
+        /*
+            usage of this will be user.on('event', callback)
+        */
+        return this.events.on;
+    }
+
+    get trigger() {
+        return this.events.trigger;
+    }
+    
+    get get() {
+        return this.attributes.get;
     }
 }
